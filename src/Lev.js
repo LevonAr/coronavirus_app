f593980dd04ca8901c56846aeff18c2f5853acac
Levon Arabyan <levonrm10@gmail.com>
2020-03-17 23:16:54 -0700

    add Lev component

---
+++ src/Lev.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Lev extends React.Component {
+    render() {
+        return (
+            <h1>"Lev is hbaby"</h1>
+        )
+    }
+}
+
+
+
+export default Lev
