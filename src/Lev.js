798aa493b2d141e7f76ca0a58a1f90425a62f378
LevonAr <37823213+LevonAr@users.noreply.github.com>
2020-03-17 23:26:08 -0700

    Update Lev.js

--- src/Lev.js
+++ src/Lev.js
@@ -3,7 +3,7 @@ import React from 'react';
 class Lev extends React.Component {
     render() {
         return (
-            <h1>"Lev is hbaby"</h1>
+            <h1>"Lev"</h1>
         )
     }
 }
