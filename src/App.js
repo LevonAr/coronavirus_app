c2db1ef2d6960e2a1488d773d102c99421f2a9a7
LevonAr <37823213+LevonAr@users.noreply.github.com>
2020-03-17 23:27:50 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,3 +3,5 @@ import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import './App.css';
+
+class App extends Component {
