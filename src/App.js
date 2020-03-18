9a871cd7c8c113239e127db57ffeb4d481b9520a
LevonAr <37823213+LevonAr@users.noreply.github.com>
2020-03-17 23:27:16 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
 import React, { Component } from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll';
