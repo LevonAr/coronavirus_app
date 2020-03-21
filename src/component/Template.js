b5d10e15c66d00d237365dbc83a47077b50fffd3
LevonAr <37823213+LevonAr@users.noreply.github.com>
2020-03-20 23:39:46 -0700

    Update Template.js

--- src/component/Template.js
+++ src/component/Template.js
@@ -8,3 +8,9 @@ import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import Container from 'react-bootstrap/Container';
 import ListGroup from 'react-bootstrap/ListGroup';
+
+import {
+  Route,
+  NavLink,
+  BrowserRouter
+} from "react-router-dom";
