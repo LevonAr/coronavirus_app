a635ddc287276aa269e163a75837d50c171fe951
LevonAr <37823213+LevonAr@users.noreply.github.com>
2020-03-21 23:43:35 -0700

    Update Template.js

--- src/component/Template.js
+++ src/component/Template.js
@@ -6,7 +6,6 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
-import Container from 'react-bootstrap/Container';
 import ListGroup from 'react-bootstrap/ListGroup';
 
 import {
