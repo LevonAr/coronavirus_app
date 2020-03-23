f3e45509f93e517a060c2f9c795d201488ad1c56
LevonAr <37823213+LevonAr@users.noreply.github.com>
2020-03-22 23:52:13 -0700

    Update canv.js

--- src/component/canv.js
+++ src/component/canv.js
@@ -3,3 +3,5 @@ var CanvasJS = require('./canvasjs.min');
 CanvasJS = CanvasJS.Chart ? CanvasJS : window.CanvasJS;
 
 class CanvasJSChart extends React.Component {
+	static _cjsContainerId = 0
+	constructor(props) {
