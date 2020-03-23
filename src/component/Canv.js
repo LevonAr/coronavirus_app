4bdcab9afdd882f274b41d13f8ba33f7a8fe104d
LevonAr <37823213+LevonAr@users.noreply.github.com>
2020-03-22 23:53:02 -0700

    Update and rename canv.js to Canv.js

--- src/component/Canv.js
+++ src/component/Canv.js
@@ -11,3 +11,7 @@ class CanvasJSChart extends React.Component {
 		this.containerProps.height = props.containerProps && props.containerProps.height ? props.containerProps.height : this.options.height ? this.options.height + "px" : "400px";
 		this.chartContainerId = "canvasjs-react-chart-container-" + CanvasJSChart._cjsContainerId++;
 	}	
+	componentDidMount() {
+		//Create Chart and Render		
+		this.chart = new CanvasJS.Chart(this.chartContainerId, this.options);
+		this.chart.render();
