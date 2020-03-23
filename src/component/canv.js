8d00cf8cc8aca379a054a65fdb1dbc378b4c1bd7
LevonAr <37823213+LevonAr@users.noreply.github.com>
2020-03-22 23:52:28 -0700

    Update canv.js

--- src/component/canv.js
+++ src/component/canv.js
@@ -5,3 +5,9 @@ CanvasJS = CanvasJS.Chart ? CanvasJS : window.CanvasJS;
 class CanvasJSChart extends React.Component {
 	static _cjsContainerId = 0
 	constructor(props) {
+		super(props);		
+		this.options = props.options ? props.options : {};		
+		this.containerProps = props.containerProps ? props.containerProps : {width: "100%", position: "relative"};
+		this.containerProps.height = props.containerProps && props.containerProps.height ? props.containerProps.height : this.options.height ? this.options.height + "px" : "400px";
+		this.chartContainerId = "canvasjs-react-chart-container-" + CanvasJSChart._cjsContainerId++;
+	}	
