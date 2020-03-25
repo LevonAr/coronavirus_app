d1b80a7b3bc33ff0532551aaa2fa0d79f0a53bcb
LevonAr <37823213+LevonAr@users.noreply.github.com>
2020-03-24 23:41:21 -0700

    Update Canv.js

--- src/component/Canv.js
+++ src/component/Canv.js
@@ -32,7 +32,7 @@ class CanvasJSChart extends React.Component {
 	}	
 	render() {		
 		//return React.createElement('div', { id: this.chartContainerId, style: this.containerProps });		
-		return <div id = {this.chartContainerId} style = {this.containerProps}/>		
+		return <div id = {this.chartContainerId} />		
 	}			
 		
 }
