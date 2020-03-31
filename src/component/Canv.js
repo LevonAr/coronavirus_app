68c3a0bb1f7dc17fde7b75c64584182b6f9bd2d5
LevonAr <37823213+LevonAr@users.noreply.github.com>
2020-03-30 23:09:31 -0700

    Update Canv.js

--- src/component/Canv.js
+++ src/component/Canv.js
@@ -13,7 +13,7 @@ class CanvasJSChart extends React.Component {
 	}	
 	shouldComponentUpdate(nextProps, nextState){
 			//Check if Chart-options has changed and determine if component has to be updated
-		(nextProps.options === this.options);
+		(.options === this.options);
 	}
 		
 	componentWillUnmount() {
