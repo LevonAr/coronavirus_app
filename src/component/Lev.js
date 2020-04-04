0060a17d1aea7be589c1fafed0a414ad2fb2d4f2
LevonAr <37823213+LevonAr@users.noreply.github.com>
2020-04-03 23:50:05 -0700

    Update Lev.js

--- src/component/Lev.js
+++ src/component/Lev.js
@@ -3,3 +3,4 @@ import React from 'react';
 const Card = ({ id }) => {
   return (
     <div className='tc grow bg-light-green br3 pa3 ma2 dib bw2 shadow-5'>
+      <img alt='robots' src={`https://robohash.org/${id}?size=200x200`} />
