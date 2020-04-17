4bc535af03bf27e1e6171f1f466e77657f2bbd4d
LevonAr <37823213+LevonAr@users.noreply.github.com>
2020-04-16 23:36:17 -0700

    Update ColumnChartStates.js

--- src/component/column_charts/ColumnChartStates.js
+++ src/component/column_charts/ColumnChartStates.js
@@ -3,3 +3,6 @@ import ReactDOM from 'react-dom';
 import { VictoryBar, VictoryChart, VictoryAxis, VictoryTheme, VictoryStack } from 'victory';
 
 const anime1 = {
+  duration: 2000,
+  onLoad: { duration: 1000 }
+}
