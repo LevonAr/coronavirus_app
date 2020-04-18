2acd093b4a0c949ccc6b489ab12b8717d126f8f2
LevonAr <37823213+LevonAr@users.noreply.github.com>
2020-04-17 23:35:52 -0700

    Update ColumnReChart.js

--- src/component/column_charts/ColumnReChart.js
+++ src/component/column_charts/ColumnReChart.js
@@ -11,3 +11,9 @@ class ColumnChartReChart extends PureComponent {
       data_array: []
     }
   }
+
+  componentDidMount() {
+    fetch("https://raw.githubusercontent.com/LevonAr/Covid-19-data-JSON/master/update1_time_series/update1_fetch.json")
+      .then((response) => {
+        return response.text();
+      })  
