9bb220fa6b3c0e67734478ef666ea375d1a09f0e
LevonAr <37823213+LevonAr@users.noreply.github.com>
2020-04-22 23:41:51 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
-import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
-import Scroll from '../components/Scroll';
+import Scroll from '../Scroll';
 import './App.css';
 
 class App extends Component {
