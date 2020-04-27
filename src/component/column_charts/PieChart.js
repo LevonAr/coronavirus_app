c686197903bb6101682e701a24c8ce1430a5ac6e
LevonAr <37823213+LevonAr@users.noreply.github.com>
2020-04-26 23:19:18 -0700

    Update PieChart.js

--- src/component/column_charts/PieChart.js
+++ src/component/column_charts/PieChart.js
@@ -2,3 +2,4 @@ import React, { PureComponent } from 'react';
 import {
   BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, Label
 } from 'recharts';
+import Button from 'react-bootstrap/Button'
