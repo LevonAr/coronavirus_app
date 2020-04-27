a95f4884a6bf283014b67848471bbe3310719f0c
LevonAr <37823213+LevonAr@users.noreply.github.com>
2020-04-26 23:21:58 -0700

    Update PieChart.js

--- src/component/column_charts/PieChart.js
+++ src/component/column_charts/PieChart.js
@@ -22,3 +22,5 @@ class ColumnChartStates extends PureComponent {
   componentDidMount() { 
     //scroll page up after clicking on state
     window.scrollTo(0, 0)
+    fetch("https://raw.githubusercontent.com/LevonAr/Covid-19-data-JSON/master/update1_time_series/USA_update1.json")
+    .then((response) => {
