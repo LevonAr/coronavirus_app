52380cef30ecf3fec12a56bb3b8c16e7c8ea6f33
LevonAr <37823213+LevonAr@users.noreply.github.com>
2020-05-08 23:58:14 -0700

    Update counties_chart.js

--- src/component/fatality_rate_chart/counties_chart.js
+++ src/component/fatality_rate_chart/counties_chart.js
@@ -101,7 +101,6 @@ class FatalityChartStates extends React.Component {
 									<>
 									<ToggleButtonGroup type="radio" name="options" defaultValue={"FatalityChart"}>
 										<ToggleButton variant="secondary" value={"FatalityChart"} onChange={this.handleClick} >Fatality Rate</ToggleButton>
-										<ToggleButton variant="secondary" value={"NewCasesChart"} onChange={this.handleClick} >Daily New Cases</ToggleButton>
 									</ToggleButtonGroup>
 									</>
 								</div>     							 
