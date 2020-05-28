295ccf07db485427d0f9ef676b4be3896413eb96
LevonAr <37823213+LevonAr@users.noreply.github.com>
2020-05-27 23:21:12 -0700

    Update Aggregator.js

--- src/component/Aggregator.js
+++ src/component/Aggregator.js
@@ -21,7 +21,7 @@ class ChartSelector extends React.Component {
   render() {
     return (
       <div id="main">
-
+      <br></br>
       <center>
       <div id="font" class="shadow p-3 mb-0 rounded bg-white border ">
         Total Confirmed Cases in {this.props.info}: {this.state.total}
