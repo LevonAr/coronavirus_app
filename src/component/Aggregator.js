c60e6e5d8da3536ecf430b6fb164070f55c70d6d
LevonAr <37823213+LevonAr@users.noreply.github.com>
2020-05-27 23:20:37 -0700

    Update Aggregator.js

--- src/component/Aggregator.js
+++ src/component/Aggregator.js
@@ -20,9 +20,10 @@ class ChartSelector extends React.Component {
   }
   render() {
     return (
-      <div>
-          {this.selectChart(this.props)}
-      </div>
+      <div id="main">
+
+      <br>
+      </br>
     )
   }
 
