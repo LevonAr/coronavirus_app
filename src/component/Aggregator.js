2c26a3919437a466c095fe44407809a97eb17080
LevonAr <37823213+LevonAr@users.noreply.github.com>
2020-05-31 23:48:56 -0700

    Update Aggregator.js

--- src/component/Aggregator.js
+++ src/component/Aggregator.js
@@ -7,7 +7,7 @@ class ChartSelector extends React.Component {
     super(props);
     this.state = {
       fatality_chart:  FatalityChart,
-      new_cases_chart: ColumnChart,
+      new_cases_chart: ColumnChartState,
 		};
     this.selectChart = this.selectChart.bind(this);
 	}
