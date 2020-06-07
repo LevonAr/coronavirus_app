6399546de34ac0cf51c1c5fb2928d3c53e91ddad
LevonAr <37823213+LevonAr@users.noreply.github.com>
2020-06-06 23:54:22 -0700

    Update Aggregator.js

--- src/component/Aggregator.js
+++ src/component/Aggregator.js
@@ -2,7 +2,7 @@ import React from 'react';
 import FatalityChart from './fatality pie charts/Fatality Pie Chart Counties';
 import ColumnChartCounty from './column charts/Column Chart County';
 
-class ChartSelector extends React.Component {
+class Aggregator extends React.Component {
 	constructor(props) {
     super(props);
     this.state = {
@@ -29,4 +29,4 @@ class ChartSelector extends React.Component {
     )
   }
 }
-export default ChartSelector;
+export default Aggregator;
